bd0f5f30dbba38344daa2deffcac4775c01295ce
maalieva-webdev <guli.maalievat@gmail.com>
2025-05-02 22:47:07 +0600

    Text

--- src/component/Main/Hi/index.jsx
+++ src/component/Main/Hi/index.jsx
@@ -12,8 +12,8 @@ function index() {
             Tehnologist
           </h1>
           <p>
-            Amet minim mollit non deserunt ullamco est <br /> sit aliqua dolor
-            do amet sint. Velit officia <br /> consequat duis enim velit mollit.
+            Minim mollit non deserunt ullamco est <br /> sit aliqua dolor do
+            amet sint. Velit officia <br /> consequat duis enim velit mollit.
             Exercitation <br /> veniam consequat sunt nostrud amet.
           </p>
           <button>Download Resume</button>
